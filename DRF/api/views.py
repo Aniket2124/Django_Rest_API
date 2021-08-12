38c5f684fc2385e5027287ebf1ad42e3ae71f000
Aniket2124 <software10@varaluae.com>
2021-08-12 14:09:31 +0530

    Added stuff for serializers

---
+++ DRF/api/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+import rest_framework
+from api.models import Student
+from django.shortcuts import render
+from.models import Student
+from.serializers import StudentSerializer
+# Create your views here.
+
+
+def student_details(request):
+    stu = Student.objects.get(id=1)
+    print(stu)
+    serializer = StudentSerializer(stu)
+    print(serializer)
+    print(serializer.data)
+    json_data = JSONRenderer().render(serializer.data)
+    print(json_data)
+    return HttpResponse(json_data, content_type='application/json')
