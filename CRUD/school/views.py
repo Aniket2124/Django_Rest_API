eb286eb2e9681a6f4418317857a2e6456720cd9f
Software10 <patilaniket04@gmail.com>
2021-12-06 09:57:45 +0530

    added snippet for reading data from dataase using rest api

---
+++ CRUD/school/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework import serializers
+from .models import Student
+import io
+from rest_framework.parsers import JSONParser
+from.serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+# Create your views here.
+def student_api(request):
+    if request.method == "GET":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id',None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type = 'application/json ')
+
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many = True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type = 'application/json ')
